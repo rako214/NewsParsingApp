79a2bee170443dd0775cc6e83cf91d5f56630286
Александр Минайчев <alexander.minaychev@gmail.com>
2020-09-14 13:07:46 +0700

    Upload .sln and code files

---
+++ ParsingApp/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ParsingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rTBRead = new System.Windows.Forms.RichTextBox();
+            this.rTBLoad = new System.Windows.Forms.RichTextBox();
+            this.btnReadNews = new System.Windows.Forms.Button();
+            this.btnLoadNewsToDB = new System.Windows.Forms.Button();
+            this.btnReadNewsFromDB = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rTBRead
+            // 
+            this.rTBRead.Location = new System.Drawing.Point(35, 47);
+            this.rTBRead.Name = "rTBRead";
+            this.rTBRead.Size = new System.Drawing.Size(326, 355);
+            this.rTBRead.TabIndex = 0;
+            this.rTBRead.Text = "";
+            // 
+            // rTBLoad
+            // 
+            this.rTBLoad.Location = new System.Drawing.Point(416, 47);
+            this.rTBLoad.Name = "rTBLoad";
+            this.rTBLoad.Size = new System.Drawing.Size(350, 355);
+            this.rTBLoad.TabIndex = 1;
+            this.rTBLoad.Text = "";
+            // 
+            // btnReadNews
+            // 
+            this.btnReadNews.Location = new System.Drawing.Point(35, 442);
+            this.btnReadNews.Name = "btnReadNews";
+            this.btnReadNews.Size = new System.Drawing.Size(151, 23);
+            this.btnReadNews.TabIndex = 2;
+            this.btnReadNews.Text = "Прочитать новости";
+            this.btnReadNews.UseVisualStyleBackColor = true;
+            this.btnReadNews.Click += new System.EventHandler(this.btnReadNews_Click);
+            // 
+            // btnLoadNewsToDB
+            // 
+            this.btnLoadNewsToDB.Location = new System.Drawing.Point(35, 485);
+            this.btnLoadNewsToDB.Name = "btnLoadNewsToDB";
+            this.btnLoadNewsToDB.Size = new System.Drawing.Size(151, 23);
+            this.btnLoadNewsToDB.TabIndex = 3;
+            this.btnLoadNewsToDB.Text = "Загрузить новости в БД";
+            this.btnLoadNewsToDB.UseVisualStyleBackColor = true;
+            this.btnLoadNewsToDB.Click += new System.EventHandler(this.btnLoadNewsToDB_Click);
+            // 
+            // btnReadNewsFromDB
+            // 
+            this.btnReadNewsFromDB.Location = new System.Drawing.Point(534, 442);
+            this.btnReadNewsFromDB.Name = "btnReadNewsFromDB";
+            this.btnReadNewsFromDB.Size = new System.Drawing.Size(156, 23);
+            this.btnReadNewsFromDB.TabIndex = 4;
+            this.btnReadNewsFromDB.Text = "Прочитать новости из БД";
+            this.btnReadNewsFromDB.UseVisualStyleBackColor = true;
+            this.btnReadNewsFromDB.Click += new System.EventHandler(this.btnReadNewsFromDB_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(317, 485);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "https://news.yandex.ru/games.rss";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 545);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnReadNewsFromDB);
+            this.Controls.Add(this.btnLoadNewsToDB);
+            this.Controls.Add(this.btnReadNews);
+            this.Controls.Add(this.rTBLoad);
+            this.Controls.Add(this.rTBRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rTBRead;
+        private System.Windows.Forms.RichTextBox rTBLoad;
+        private System.Windows.Forms.Button btnReadNews;
+        private System.Windows.Forms.Button btnLoadNewsToDB;
+        private System.Windows.Forms.Button btnReadNewsFromDB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ParsingApp/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using MySql.Data.MySqlClient;
+
+
+namespace ParsingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Массив строк для чтения новостей
+        /// </summary>
+        private string[] ReadNews;
+        /// <summary>
+        /// Счётчики строк для айди, тайтла, линков, описания, пабдаты
+        /// </summary>
+        private int idc = 0;
+        private int titlec = 0;
+        private int linkc = 2;
+        private int descc = 4;
+        private int pubdatec = 6;
+        private int linesc = 0;
+
+        /// <summary>
+        /// Кнопка чтения новостей из ленты
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReadNews_Click(object sender, EventArgs e)
+        {
+            rTBRead.Text = "";
+            ReadNews = ReadPage();
+        }
+        /// <summary>
+        /// Кнопка загрузки новостей в БД
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLoadNewsToDB_Click(object sender, EventArgs e)
+        {
+            //Запрос для удаления прошлого сохранения
+            string queryDelete = "DELETE FROM news WHERE 1";
+            //Запрос для записи данных
+            string queryInsert = "INSERT INTO news (id, title, link, description, pubdate) VALUES (@id, @title, @link, @description, @pubdate)";
+
+            MySqlConnection connectionLoadToDB = StartNewSqlConnection();
+            MySqlCommand commandLoadToDB = new MySqlCommand(queryDelete, connectionLoadToDB);
+
+            commandLoadToDB.ExecuteNonQuery();
+
+            //Записываем данные в БД
+            do
+            {
+                linesc = WriteToDB(queryInsert, connectionLoadToDB);
+            } while (linesc < ReadNews.Length);
+
+            connectionLoadToDB.Close();
+        
+
+        }
+
+        private void btnReadNewsFromDB_Click(object sender, EventArgs e)
+        {
+
+            MySqlConnection connectionRead = StartNewSqlConnection();
+
+            string query = "SELECT * FROM news";
+            MySqlCommand command = new MySqlCommand(query, connectionRead);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                rTBLoad.AppendText(reader[1] + "\n\n");
+                rTBLoad.AppendText(reader[2] + "\n\n");
+                rTBLoad.AppendText(reader[3] + "\n\n");
+                rTBLoad.AppendText(reader[4].ToString() + "\n\n");
+            }
+
+            connectionRead.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Reads whole rss page and returns string[] of news
+        /// </summary>
+        /// <returns>string[]newsText</returns>
+        private string[] ReadPage()
+        {
+            string uri = textBox1.Text;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string text = reader.ReadToEnd();
+
+            string[] tagTrash = { "<title>", "</title>", "<link>", "</link>", "<description>", "</description>", "<pubDate>", "</pubDate>"};
+            string[] newsText = text.Split(tagTrash, System.StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 11; i < newsText.Length; i = i + 2)
+            {
+               newsText[i] = newsText[i].Replace("+0000", "");
+               rTBRead.AppendText(newsText[i] + "\n\n");
+            }
+            return newsText;
+        }
+
+        /// <summary>
+        /// Writes all data to DB        
+        /// </summary>
+        /// <param name="connection"></param>
+        private int WriteToDB(string query, MySqlConnection connectionLoadToDB)
+        {
+            MySqlCommand commandWriteElementsToDB = new MySqlCommand(query, connectionLoadToDB);
+            commandWriteElementsToDB.Parameters.Add("@id", MySqlDbType.Int32).Value = idc;
+            commandWriteElementsToDB.Parameters.Add("@title", MySqlDbType.Text).Value = rTBRead.Lines[titlec];
+            commandWriteElementsToDB.Parameters.Add("@link", MySqlDbType.Text).Value = rTBRead.Lines[linkc];
+            commandWriteElementsToDB.Parameters.Add("@description", MySqlDbType.Text).Value = rTBRead.Lines[descc];
+            commandWriteElementsToDB.Parameters.Add("@pubDate", MySqlDbType.Text).Value = rTBRead.Lines[pubdatec];
+            commandWriteElementsToDB.ExecuteNonQuery();
+
+            idc += 1;
+            titlec += 8;
+            linkc += 8;
+            descc += 8;
+            pubdatec += 8;
+            linesc += 9;
+
+            return linesc;
+        }
+
+        private MySqlConnection StartNewSqlConnection()
+        {
+            string connectionString = "server = localhost; user = root; database = news; password = ''; Charset = UTF8;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
